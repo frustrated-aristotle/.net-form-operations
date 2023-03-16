6414e58c57a23038ca468c068dd8c733a75cdf82
qersha <a.secgeler.imp@gmail.com>
2023-03-16 15:16:40 +0300

    Input Data Truth Check

--- form-op/Form1.Designer.cs
+++ form-op/Form1.Designer.cs
@@ -29,13 +29,16 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxUserName = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.button1 = new System.Windows.Forms.Button();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.label3 = new System.Windows.Forms.Label();
+            this.textBoxCaptchaInput = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxCaptcha = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -49,38 +52,39 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "User Name:";
             // 
-            // textBox1
+            // textBoxUserName
             // 
-            this.textBox1.Location = new System.Drawing.Point(223, 124);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(212, 35);
-            this.textBox1.TabIndex = 2;
+            this.textBoxUserName.Location = new System.Drawing.Point(223, 124);
+            this.textBoxUserName.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.textBoxUserName.Name = "textBoxUserName";
+            this.textBoxUserName.Size = new System.Drawing.Size(212, 35);
+            this.textBoxUserName.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Roboto Mono", 15F);
-            this.label2.Location = new System.Drawing.Point(79, 197);
+            this.label2.Location = new System.Drawing.Point(91, 197);
             this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(132, 26);
+            this.label2.Size = new System.Drawing.Size(120, 26);
             this.label2.TabIndex = 5;
-            this.label2.Text = "User Name:";
+            this.label2.Text = "Password:";
             // 
-            // textBox2
+            // textBoxPassword
             // 
-            this.textBox2.Location = new System.Drawing.Point(223, 193);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(212, 35);
-            this.textBox2.TabIndex = 6;
+            this.textBoxPassword.Location = new System.Drawing.Point(223, 193);
+            this.textBoxPassword.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(212, 35);
+            this.textBoxPassword.TabIndex = 6;
+            this.textBoxPassword.UseSystemPasswordChar = true;
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
             this.checkBox1.Font = new System.Drawing.Font("Roboto Mono", 13F);
-            this.checkBox1.Location = new System.Drawing.Point(240, 374);
+            this.checkBox1.Location = new System.Drawing.Point(240, 402);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(150, 28);
             this.checkBox1.TabIndex = 7;
@@ -89,19 +93,20 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(263, 269);
+            this.button1.Location = new System.Drawing.Point(263, 309);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(127, 35);
             this.button1.TabIndex = 8;
             this.button1.Text = "Login";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
             this.linkLabel1.Font = new System.Drawing.Font("Roboto Mono", 13F);
             this.linkLabel1.LinkColor = System.Drawing.Color.Gray;
-            this.linkLabel1.Location = new System.Drawing.Point(219, 332);
+            this.linkLabel1.Location = new System.Drawing.Point(219, 365);
             this.linkLabel1.Name = "linkLabel1";
             this.linkLabel1.Size = new System.Drawing.Size(230, 24);
             this.linkLabel1.TabIndex = 9;
@@ -119,19 +124,50 @@
             this.label3.TabIndex = 10;
             this.label3.Text = "Admin Login Form";
             // 
+            // textBoxCaptchaInput
+            // 
+            this.textBoxCaptchaInput.Location = new System.Drawing.Point(223, 262);
+            this.textBoxCaptchaInput.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.textBoxCaptchaInput.Name = "textBoxCaptchaInput";
+            this.textBoxCaptchaInput.Size = new System.Drawing.Size(86, 35);
+            this.textBoxCaptchaInput.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Roboto Mono", 15F);
+            this.label4.Location = new System.Drawing.Point(103, 266);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 26);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Captcha:";
+            // 
+            // textBoxCaptcha
+            // 
+            this.textBoxCaptcha.Enabled = false;
+            this.textBoxCaptcha.Location = new System.Drawing.Point(349, 262);
+            this.textBoxCaptcha.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.textBoxCaptcha.Name = "textBoxCaptcha";
+            this.textBoxCaptcha.Size = new System.Drawing.Size(86, 35);
+            this.textBoxCaptcha.TabIndex = 13;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 28F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.ClientSize = new System.Drawing.Size(561, 432);
+            this.Controls.Add(this.textBoxCaptcha);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxCaptchaInput);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBoxPassword);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxUserName);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Roboto Mono", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
@@ -140,6 +176,7 @@
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -148,13 +185,16 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxUserName;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBoxPassword;
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.LinkLabel linkLabel1;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxCaptchaInput;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxCaptcha;
     }
 }
 
--- form-op/Form1.cs
+++ form-op/Form1.cs
@@ -16,5 +16,32 @@ namespace form_op
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBoxUserName.Text == "admin" && textBoxPassword.Text == "1234" && textBoxCaptchaInput.Text == textBoxCaptcha.Text)
+            {
+                Form2 secondForm = new Form2();
+                secondForm.ShowDialog();
+                this.Hide();
+            }
+            else
+            {
+                
+                MessageBox.Show("Wrong Inputs!", "Info" , MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CreateCaptcha();
+        }
+        
+        private void CreateCaptcha()
+        {
+            Random rnd = new Random();
+            int captchaInt = rnd.Next(10000, 100000);
+            textBoxCaptcha.Text = captchaInt.ToString();    
+        }
     }
 }
